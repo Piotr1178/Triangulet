3ec17cea4113498abecbb0ee138637383b1d0844
Piotr <89880037+Piotr1178@users.noreply.github.com>
2023-05-31 18:58:33 -0400

    Update opencapsules.js

--- hacks/opencapsules.js
+++ hacks/opencapsules.js
@@ -10,73 +10,107 @@ let unlocks = {
 let unique = 'NONE'
 let stop = false
 let opened = 0
-
 let packs = await fetch('/data/trians', { headers: { 'Accept': 'application/json', 'authorization': triangulet.tokenraw, 'Content-Type': 'application/json' } }).then(x => {return x.json()})
-
 packOptions = await packs.ValuesnCapsules.map(x => x.name)
+let trianguletClient
+
+let marketOpened = true
+if (window.location.pathname.split('/')[1] != 'market') marketOpened = false
 
 let pack = prompt(`What pack do you want to open? These are the following options: ${packOptions.join(', ')}`)
 if (!packOptions.includes(pack)) {
     alert('This is not a valid capsule. Currently, this is case sensitive (Will change in the future)')
 } else {
-    el = document.createElement('div')
-el.style.position = 'absolute'
-el.style.top = '10px'
-el.style.right = '10px'
-el.style.height = 'fit-content'
-el.style.width = '250px'
-el.style.padding = '18px'
-el.style.background = '#d1d1d1'
-el.style.border = '2px solid rgba(0,0,0,0.6)'
-el.style.borderRadius = '15px'
-el.style.zIndex = '1000'
-el.classList.add('triangulet-hack-box')
-el.style.display = 'flex'
-el.style.flexDirection = 'column'
-el.style.boxShadow = '0px 0px 11px 0px rgba(0,0,0,0.75)'
-document.querySelector('body').appendChild(el)
-
-function run() {
-    if (stop) {
-        stop = false
-        document.querySelector('.openerStop').innerHTML = 'Stop Opener'
+    if (!marketOpened) {
+        alert('You need to be on the market page. The script will automatically run on the other tab. DO NOT CLOSE THIS TAB!')
+        trianguletClient = window.open('/market')
     }
-    else {
-        stop = true
-        document.querySelector('.openerStop').innerHTML = 'Start Opener'
-    }
-}
-
-
-
-setInterval(() => {
-    if (!stop) {
-        fetch('/api/open', { method: 'POST', headers: { 'Accept': 'application/json', 'authorization': triangulet.tokenraw, 'Content-Type': 'application/json' }, body: JSON.stringify({ "capsule": pack })}).then(x => x.json()).then(response => {
-                if (response.new) unique = response.trian
-                if (unlocks[response.rarity][response.trian]) unlocks[response.rarity][response.trian] ++
-                else unlocks[response.rarity][response.trian] = 1
-                opened ++
-                document.querySelector('.triangulet-hack-box').innerHTML = ''
-                document.querySelector('.triangulet-hack-box').innerHTML += `<h3 style="margin: 10px">${response.rarity} ${response.trian} (${unlocks[response.rarity][response.trian].toLocaleString()}x)</h3>`
-                document.querySelector('.triangulet-hack-box').innerHTML += `<p style="margin: 0px">Coded by <a href="https://github.com/Piotr1178/Triangulet">Piotr</a>`
-                document.querySelector('.triangulet-hack-box').innerHTML += `<p style="margin: 0px">RECENT UNIQUE: ${unique}</p>`
-                document.querySelector('.triangulet-hack-box').innerHTML += `<p style="margin: 0px">CAPSULES OPENED: ${opened.toLocaleString()}</p>`
-                document.querySelector('.triangulet-hack-box').innerHTML += `<button type="button" class="openerStop" style="background-color: #039162; color: white; padding: 9px; border-radius: 8px; height: fit-content; width: fit-content; font-size: 15px; font-family: Nunito; border: none; box-shadow: 0px 7px 0px 0px #01744e; cursor: pointer; transition: .3s ease; margin: 9px;" onclick="run()">Stop Opener</button>`
-                document.querySelector('.triangulet-hack-box').innerHTML += '<h2 style="margin: 3px; text-decoration: underline">Logs</h2>'
-                Object.keys(unlocks).forEach(rarity => {
-                    document.querySelector('.triangulet-hack-box').innerHTML += `<h3 style="margin: 3px">${rarity}</h3>`
-                    Object.keys(unlocks[rarity]).forEach(ul => {
-                        document.querySelector('.triangulet-hack-box').innerHTML += `<p style="margin: 0">${ul}: ${unlocks[rarity][ul].toLocaleString()}x</p>`
-                    })
-                })
-            })
+    setTimeout(() => {
+        if (marketOpened) {
+            el = document.createElement('div')
+            el.style.position = 'absolute'
+            el.style.bottom = '10px'
+            el.style.right = '10px'
+            el.style.height = 'fit-content'
+            el.style.width = '250px'
+            el.style.padding = '18px'
+            el.style.background = '#6de332'
+            el.style.border = '2px solid rgba(0,0,0,0.3)'
+            el.style.borderRadius = '15px'
+            el.style.zIndex = '1000'
+            el.classList.add('triangulet-hack-box')
+            el.style.display = 'flex'
+            el.style.flexDirection = 'column'
+            el.style.textAlign = 'center'
+            el.style.boxShadow = '0px 0px 15px 0px rgba(0,0,0,0.8)'
+            el.style.overflow = 'hidden'
+            document.querySelector('body').appendChild(el)
+            dragElement(document.querySelector('.triangulet-hack-box'));
+        } else {
+            el = trianguletClient.document.createElement('div')
+            el.style.position = 'absolute'
+            el.style.bottom = '10px'
+            el.style.right = '10px'
+            el.style.height = 'fit-content'
+            el.style.width = '250px'
+            el.style.padding = '18px'
+            el.style.background = '#6de332'
+            el.style.border = '2px solid rgba(0,0,0,0.3)'
+            el.style.borderRadius = '15px'
+            el.style.zIndex = '1000'
+            el.classList.add('triangulet-hack-box')
+            el.style.display = 'flex'
+            el.style.flexDirection = 'column'
+            el.style.textAlign = 'center'
+            el.style.boxShadow = '0px 0px 15px 0px rgba(0,0,0,0.8)'
+            el.style.overflow = 'hidden'
+            trianguletClient.document.querySelector('body').appendChild(el)
+            dragElement(trianguletClient.document.querySelector('.triangulet-hack-box'));
         }
-    }, 1150)
+
+        setInterval(() => {
+    if (!stop) {
+                fetch('/api/open', { method: 'POST', headers: { 'Accept': 'application/json', 'authorization': triangulet.tokenraw, 'Content-Type': 'application/json' }, body: JSON.stringify({ "capsule": pack })}).then(x => x.json()).then(response => {
+                        if (unlocks[response.rarity][response.trian]) unlocks[response.rarity][response.trian] ++
+                        else unlocks[response.rarity][response.trian] = 1
+                        opened ++
+                        if (marketOpened) {
+                            document.querySelector('.triangulet-hack-box').innerHTML = ''
+                            document.querySelector('.triangulet-hack-box').innerHTML += `<h3 style="margin: 10px">${response.rarity} ${response.trian} (${unlocks[response.rarity][response.trian].toLocaleString()}x)</h3>`
+                            document.querySelector('.triangulet-hack-box').innerHTML += `<p style="margin: 0px">Coded by <a href="https://github.com/Piotr1178/Triangulet">Piotr</a>`
+                            document.querySelector('.triangulet-hack-box').innerHTML += `<p style="margin: 0px">CAPSULES OPENED: <b>${opened.toLocaleString()}</b></p>`
+                            document.querySelector('.triangulet-hack-box').innerHTML += '<h2 style="margin: 3px; text-decoration: underline">Logs</h2>'
+                        } else {
+                            trianguletClient.document.querySelector('.triangulet-hack-box').innerHTML = ''
+                            trianguletClient.document.querySelector('.triangulet-hack-box').innerHTML += `<h3 style="margin: 10px">${response.rarity} ${response.trian} (${unlocks[response.rarity][response.trian].toLocaleString()}x)</h3>`
+                            trianguletClient.document.querySelector('.triangulet-hack-box').innerHTML += `<p style="margin: 0px">Coded by <a href="https://github.com/Piotr1178/Triangulet">Piotr</a>`
+                            trianguletClient.document.querySelector('.triangulet-hack-box').innerHTML += `<p style="margin: 0px">CAPSULES OPENED: <b>${opened.toLocaleString()}</b></p>`
+                            trianguletClient.document.querySelector('.triangulet-hack-box').innerHTML += '<h2 style="margin: 3px; text-decoration: underline">Logs</h2>'
+                        }
+                        Object.keys(unlocks).forEach(rarity => {
+                            let percentageUnlocks = 0
+                            Object.keys(unlocks[rarity]).forEach(trian => {
+                                percentageUnlocks = percentageUnlocks + unlocks[rarity][trian]
+                            })
+                            percentageUnlocks = ((percentageUnlocks/opened)*100).toFixed(3);
+                            if (marketOpened) document.querySelector('.triangulet-hack-box').innerHTML += `<h3 style="margin: 3px">${rarity} (${percentageUnlocks}%)</h3>`
+                            else trianguletClient.document.querySelector('.triangulet-hack-box').innerHTML += `<h3 style="margin: 3px">${rarity} (${percentageUnlocks}%)</h3>`
+                            Object.keys(unlocks[rarity]).forEach(ul => {
+                                if (marketOpened) document.querySelector('.triangulet-hack-box').innerHTML += `<p style="margin: 0">${ul}: <b>${unlocks[rarity][ul].toLocaleString()}x</b></p>`
+                                else trianguletClient.document.querySelector('.triangulet-hack-box').innerHTML += `<p style="margin: 0">${ul}: <b>${unlocks[rarity][ul].toLocaleString()}x</b></p>`
+                            })
+                        })
+                    })
+                }
+            fetch('/data/user', { headers: { 'Accept': 'application/json', 'authorization': triangulet.tokenraw, 'Content-Type': 'application/json' } }).then(x => {return x.json()}).then(user => {
+                if (marketOpened) document.querySelector('#tokenbalance').innerHTML = user.tokens.toLocaleString()
+                else trianguletClient.document.querySelector('#tokenbalance').innerHTML = user.tokens.toLocaleString()
+            })
+            }, 1150)
+        }, 1000)
 }
 
 // was lazy to write it lol https://www.w3schools.com/howto/howto_js_draggable.asp
-
-dragElement(document.querySelector('.triangulet-hack-box'));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
