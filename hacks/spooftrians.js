40d190354ed166f5903f012c05556b5b301c41c7
Piotr <89880037+Piotr1178@users.noreply.github.com>
2023-05-31 20:18:45 -0400

    Update spooftrians.js

--- hacks/spooftrians.js
+++ hacks/spooftrians.js
@@ -14,7 +14,7 @@ let color = {
                 capsulesReached = capsulesReached + 1
                 let triansAdded = []
                 capsule.trians.forEach(trian => {
-                    triansAdded.push(`<div class="styles__blookContainer___3JrKb-camelCase" role="button" tabindex="0"><div class="styles__blookContainer___36LK2-camelCase styles__blook___bNr_t-camelCase" id="${trian.name}"><img draggable="false" src="/media/capsules/${capsule.name}/trians/${trian.name}.png" class="styles__blook___1R6So-camelCase"></div><div class="styles__blookText___3AMdK-camelCase" id="triannumcircle${trian.name}" style="background-color: ${color[trian['rarity']]}">1</div></div>`)
+                    triansAdded.push(`<div class="styles__blookContainer___3JrKb-camelCase" role="button" tabindex="0" onclick="viewTrian('${trian.name}', '${trian.rarity}', '${capsule.name}')"><div class="styles__blookContainer___36LK2-camelCase styles__blook___bNr_t-camelCase" id="${trian.name}"><img draggable="false" src="/media/capsules/${capsule.name}/trians/${trian.name}.png" class="styles__blook___1R6So-camelCase"></div><div class="styles__blookText___3AMdK-camelCase" id="triannumcircle${trian.name}" style="background-color: ${color[trian['rarity']]}">1</div></div>`)
                 })
                 document.querySelectorAll('.styles__left___9beun-camelCase > #trianscontainer > .styles__setHolder___rVq3Z-camelCase > .styles__setBlooks___3xamH-camelCase')[capsulesReached].innerHTML = triansAdded.join('')
             })
@@ -29,13 +29,22 @@ let color = {
                     capsulesReached = capsulesReached + 1
                     let triansAdded = []
                     capsule.trians.forEach(trian => {
-                        triansAdded.push(`<div class="styles__blookContainer___3JrKb-camelCase" role="button" tabindex="0"><div class="styles__blookContainer___36LK2-camelCase styles__blook___bNr_t-camelCase" id="${trian.name}"><img draggable="false" src="/media/capsules/${capsule.name}/trians/${trian.name}.png" class="styles__blook___1R6So-camelCase"></div><div class="styles__blookText___3AMdK-camelCase" id="triannumcircle${trian.name}" style="background-color: ${color[trian['rarity']]}">1</div></div>`)
+                        triansAdded.push(`<div class="styles__blookContainer___3JrKb-camelCase" role="button" tabindex="0" onclick="viewTrian('${trian.name}', '${trian.rarity}', '${capsule.name}')"><div class="styles__blookContainer___36LK2-camelCase styles__blook___bNr_t-camelCase" id="${trian.name}"><img draggable="false" src="/media/capsules/${capsule.name}/trians/${trian.name}.png" class="styles__blook___1R6So-camelCase"></div><div class="styles__blookText___3AMdK-camelCase" id="triannumcircle${trian.name}" style="background-color: ${color[trian['rarity']]}">1</div></div>`)
                     })
                     trianguletClient.document.querySelectorAll('.styles__left___9beun-camelCase > #trianscontainer > .styles__setHolder___rVq3Z-camelCase > .styles__setBlooks___3xamH-camelCase')[capsulesReached].innerHTML = triansAdded.join('')
                 })
                 trianguletClient.alert(`Spoofed all trians!`)
             }, 1000)
         }
+
+function viewTrian(trian, rarity, capsule) {
+    document.querySelector('#trianbackground').src = `/media/capsules/${capsule}/background.png`
+    document.querySelector('#trianname').innerHTML = trian
+    document.querySelector('#trianrarity').innerHTML = rarity
+    document.querySelector('#trianrarity').style.color = color[rarity]
+    document.querySelector('#trianquantity').innerHTML = '1 Owned'
+    document.querySelector('#trianimg').src = `/media/capsules/${capsule}/trians/${trian}.png`
+}
 
 // Thanks for supporting me and my hacks: https://discord.gg/SvEhNNJWB2
 console.log('%cThank you for using my hacks! 👍', 'font-size: 25px')
